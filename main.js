08c562d9a76e6aa79fee3a1ddbc38c9c761a9a25
Rashidbek <rasishidbekrasuljonov83>
2026-04-16 14:19:41 +0300

    1-masala

---
+++ main.js
@@ -0,0 +1,10 @@
+// 1-masala sonning juft yoki toq son ekanligini aniqlovchi dastur yozing
+//===============================================
+
+let son = 3;
+
+if (son % 2 === 0) {
+  console.log("juft son");
+} else {
+  console.log("toq son");
+}
